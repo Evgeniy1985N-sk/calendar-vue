14840bc769d2508510cb66b2970c92021cc27d7b
Evgeny <evgeniylopukhin@gmail.com>
2025-11-13 09:44:41 +0300

    go to month

--- src/components/Calendar.vue
+++ src/components/Calendar.vue
@@ -24,12 +24,12 @@ onMounted(() => {
   weekDays()
 })
 
-function prevMonth() {
+function goToPrevMonth() {
   dateCurrent.value.setMonth(dateCurrent.value.getMonth() - 1);
   updateMonth()
 }
 
-function nextMonth() {
+function goToNextMonth() {
   dateCurrent.value.setMonth(dateCurrent.value.getMonth() + 1);
   updateMonth()
 }
@@ -124,7 +124,7 @@ function updateYear() {
     <div class="calendar">
 
       <div class="months">
-        <div @click="prevMonth" class="arrow">
+        <div @click="goToPrevMonth" class="arrow">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
             stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M6.75 15.75 3 12m0 0 3.75-3.75M3 12h18" />
@@ -133,7 +133,7 @@ function updateYear() {
         <p>
           {{ month }} {{ year }}
         </p>
-        <div @click="nextMonth" class="arrow arrow_right">
+        <div @click="goToNextMonth" class="arrow arrow_right">
           <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5"
             stroke="currentColor" class="size-6">
             <path stroke-linecap="round" stroke-linejoin="round" d="M17.25 8.25 21 12m0 0-3.75 3.75M21 12H3" />
